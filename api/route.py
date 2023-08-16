8b028b26e6bacb5d429c8022b2420ef149fa60e3
garfunkel <garfunkel@126.com>
2023-08-17 01:36:32 +0800

    test 0817

--- api/route.py
+++ api/route.py
@@ -19,40 +19,40 @@ def admin_api(apiName):
     # if request_data.get("action") == "" or request_data.get("action") is None:
     #     return jsonify({'code': 0})
     # else:
-    match apiName:
-        case 'admin':
-            currClass = admin.AdminAdmin(request_data.get("action"))
-        case 'store':
-            if request.args.get('action') == 'uploadImg':
-                request_data["action"] = 'uploadImg'
-
-            currClass = store.StoreAdmin(request_data.get("action"))
-        case 'user':
-            currClass = user.UserAdmin(request_data.get('action'))
-        case 'orders':
-            currClass = orders.OrdersAdmin(request_data.get('action'))
-        case _:
-            currClass = None
-
-    resp_data = currClass.init_data(request_data)
-    print(resp_data)
-    return jsonify(resp_data)
+    # match apiName:
+    #     case 'admin':
+    #         currClass = admin.AdminAdmin(request_data.get("action"))
+    #     case 'store':
+    #         if request.args.get('action') == 'uploadImg':
+    #             request_data["action"] = 'uploadImg'
+    #
+    #         currClass = store.StoreAdmin(request_data.get("action"))
+    #     case 'user':
+    #         currClass = user.UserAdmin(request_data.get('action'))
+    #     case 'orders':
+    #         currClass = orders.OrdersAdmin(request_data.get('action'))
+    #     case _:
+    #         currClass = None
+    #
+    # resp_data = currClass.init_data(request_data)
+    # print(resp_data)
+    # return jsonify(resp_data)
 
 
 @app.route('/api/<apiName>', methods=['GET', 'POST'])
 def api_port(apiName):
     request_data = request.form.to_dict()
     print("api", apiName)
-    match apiName:
-        case 'user':
-            currClass = user.UserApi(request_data.get("action"))
-        case 'store':
-            currClass = store.StoreApi(request_data.get("action"))
-        case 'orders':
-            currClass = orders.OrdersApi(request_data.get('action'))
-        case _:
-            currClass = None
-
-    resp_data = currClass.init_data(request_data)
-    print(resp_data)
-    return jsonify(resp_data)
+    # match apiName:
+    #     case 'user':
+    #         currClass = user.UserApi(request_data.get("action"))
+    #     case 'store':
+    #         currClass = store.StoreApi(request_data.get("action"))
+    #     case 'orders':
+    #         currClass = orders.OrdersApi(request_data.get('action'))
+    #     case _:
+    #         currClass = None
+    #
+    # resp_data = currClass.init_data(request_data)
+    # print(resp_data)
+    # return jsonify(resp_data)
